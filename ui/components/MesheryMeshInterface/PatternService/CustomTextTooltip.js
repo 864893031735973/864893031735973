0fd6080e6182df13e9c53f77a6ca4e787e1c5e12
864893031735973 <63679801+864893031735973@users.noreply.github.com>
2024-06-16 08:19:08 +0330

    Initialize fix/tooltip

---
+++ ui/components/MesheryMeshInterface/PatternService/CustomTextTooltip.js
@@ -0,0 +1,43 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { CustomTooltip } from '@layer5/sistent';
+import { UsesSistent } from '@/components/SistentWrapper';
+
+const useStyles = makeStyles((theme) => ({
+  toolTip: {
+    textDecoration: 'underline',
+    color: theme.palette.secondary.link2,
+    cursor: 'pointer',
+  },
+}));
+
+export const RenderTooltipContent = ({ showPriortext, showAftertext, link }) => {
+  const classes = useStyles();
+
+  const handleClick = (e) => {
+    window.open(link, '_blank');
+    e.stopPropagation();
+  };
+
+  return (
+    <div>
+      <span style={{ marginRight: '2px' }}>{showPriortext}</span>
+      <a
+        onClick={handleClick}
+        className={classes.toolTip}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn more
+      </a>
+      <span style={{ marginLeft: '2px' }}>{showAftertext}</span>
+    </div>
+  );
+};
+
+export const CustomTextTooltip = () => {
+  return (
+    <UsesSistent>
+      <CustomTooltip />
+    </UsesSistent>
+  );
+};
